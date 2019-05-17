88c1e66ee4b77028e47975e5a1626929bac6b7f7
Ali Rahighi <tizpardazesh@gmail.com>
2019-05-17 19:21:56 +0430

    Add project files.

---
+++ OfficeHelper/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace OfficeHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalTheme1 = new Telerik.WinControls.Themes.CrystalTheme();
+            this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radProgressBar1 = new Telerik.WinControls.UI.RadProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radProgressBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Word documents (*.docx)|";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(117, 146);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(130, 69);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "Select docx file !";
+            this.radButton1.ThemeName = "VisualStudio2012Dark";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(57, 252);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(244, 50);
+            this.radButton2.TabIndex = 1;
+            this.radButton2.Text = "START";
+            this.radButton2.ThemeName = "VisualStudio2012Dark";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radProgressBar1
+            // 
+            this.radProgressBar1.Location = new System.Drawing.Point(-2, 319);
+            this.radProgressBar1.Name = "radProgressBar1";
+            this.radProgressBar1.Size = new System.Drawing.Size(367, 39);
+            this.radProgressBar1.TabIndex = 2;
+            this.radProgressBar1.ThemeName = "VisualStudio2012Dark";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(12, 12);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(332, 18);
+            this.radLabel1.TabIndex = 3;
+            this.radLabel1.Text = "Notation: Be careful that  your Microsoft Office must be activated";
+            this.radLabel1.ThemeName = "VisualStudio2012Dark";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(15, 33);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(119, 18);
+            this.radLabel2.TabIndex = 3;
+            this.radLabel2.Text = "before you could start.";
+            this.radLabel2.ThemeName = "VisualStudio2012Dark";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 355);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radProgressBar1);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.MaximumSize = new System.Drawing.Size(371, 388);
+            this.Name = "MainForm";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "MainForm";
+            this.ThemeName = "VisualStudio2012Dark";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radProgressBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.CrystalTheme crystalTheme1;
+        private Telerik.WinControls.Themes.VisualStudio2012DarkTheme visualStudio2012DarkTheme1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadProgressBar radProgressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+    }
+}
---
+++ OfficeHelper/MainForm.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+namespace OfficeHelper
+{
+    public partial class MainForm : Telerik.WinControls.UI.RadForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += BackgroundWorker1_ProgressChanged;
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+             radProgressBar1.Value1 =e.ProgressPercentage ;
+            radProgressBar1.Text = $"{e.ProgressPercentage} %";
+        }
+
+        public string FilePath { get; set; }
+        public Dictionary<string, string> Survay { get; set; }
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePath =  openFileDialog1.FileName;
+                MessageBox.Show("Ok press START now");
+            }
+
+
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+
+
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+         
+        }
+        Microsoft.Office.Interop.Excel.Application oXL;
+        Microsoft.Office.Interop.Excel._Workbook oWB;
+        Microsoft.Office.Interop.Excel._Worksheet oSheet;
+        Microsoft.Office.Interop.Excel.Range oRng;
+        object misvalue = System.Reflection.Missing.Value;
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            //MessageBox.Show("Please be patient.It may take some time depending on your pc resources");
+            Application application = new Application();
+            Document document = application.Documents.Open(FilePath);
+            Survay = new Dictionary<string, string>();
+            var validList = new List<string>();
+
+            radProgressBar1.Maximum = 100;
+
+            radProgressBar1.Step = 1;
+            radProgressBar1.Value1 = 0;
+            var c = document.Paragraphs.Count;
+            // Loop through all words in the document.
+            try
+            {
+                for (int i = 1; i <= c; i++)
+                {
+                    if (worker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        application.Quit();
+                        return;
+                        break;
+                    }
+                    if (document.Paragraphs[i].Range.Text != "\r")
+                    {
+                        validList.Add(document.Paragraphs[i].Range.Text.Replace("\r", ""));
+                    }
+                    worker.ReportProgress(i * 100 / c);
+
+
+                }
+                int count = validList.Count;
+                for (int i = 0; i < count; i += 4)
+                {
+                    var index = 0;
+                    for (int j = i; j < i + 4; j++)
+                    {
+                        //is number vote?
+                        if (index == 2 && Regex.IsMatch(validList[j], @"^\d+$"))
+                        {
+                            if (!Survay.ContainsKey(validList[j - 1]))
+                            {
+                                Survay.Add(validList[j - 1], validList[j]);
+
+                            }
+                        }
+                        index++;
+                    }
+                    // Write the word.
+                }
+
+                SaveToExcel(Survay);
+
+
+                // Close word.
+                application.Quit();
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "//errLog.txt", ex.ToString());
+                application.Quit();
+
+            }
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (worker.CancellationPending == true)
+            //    {
+            //        e.Cancel = true;
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        // Perform a time consuming operation and report progress.
+            //        System.Threading.Thread.Sleep(500);
+            //    }
+            //}
+        }
+        private void SaveToExcel(Dictionary<string, string> survay)
+        {
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+            oXL.UserControl = false;
+            //Get a new workbook.
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+            oSheet.Cells[1, 1] = "User";
+            oSheet.Cells[1, 2] = "Vote";
+
+            var index = 0;
+            foreach (var item in survay)
+            {
+                oSheet.Cells[index + 2, 1] = item.Key;
+                oSheet.Cells[index + 2, 2] = item.Value;
+
+                index++;
+
+
+            }
+
+            //Add table headers going cell by cell.
+
+
+
+            //Format A1:D1 as bold, vertical alignment = center.
+            //oSheet.get_Range("A1", "D1").Font.Bold = true;
+            //oSheet.get_Range("A1", "D1").VerticalAlignment =
+            //    Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+            // Create an array to multiple values at once.
+            //string[,] saNames = new string[5, 2];
+
+            //saNames[0, 0] = "John";
+            //saNames[0, 1] = "Smith";
+            //saNames[1, 0] = "Tom";
+
+            //saNames[4, 1] = "Johnson";
+
+            ////Fill A2:B6 with an array of values (First and Last Names).
+            //oSheet.get_Range("A2", "B6").Value2 = saNames;
+
+            ////Fill C2:C6 with a relative formula (=A2 & " " & B2).
+            //oRng = oSheet.get_Range("C2", "C6");
+            //oRng.Formula = "=A2 & \" \" & B2";
+
+            ////Fill D2:D6 with a formula(=RAND()*100000) and apply format.
+            //oRng = oSheet.get_Range("D2", "D6");
+            //oRng.Formula = "=RAND()*100000";
+            //oRng.NumberFormat = "$0.00";
+
+            ////AutoFit columns A:D.
+            //oRng = oSheet.get_Range("A1", "D1");
+            //oRng.EntireColumn.AutoFit();
+
+
+            oWB.SaveAs("c:\\test505.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            oWB.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                backgroundWorker1.CancelAsync();
+            }
+        }
+    }
+}
